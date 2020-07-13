f63dfed2d998bbacf6dc7e529505ab74835b2b49
John Reese <john@noswap.com>
2020-07-13 11:07:20 -0700

    Version bump v20200713

--- diff_match_patch/__init__.py
+++ diff_match_patch/__init__.py
@@ -5,5 +5,5 @@ if sys.version_info >= (3, 0):
 else:
     from .diff_match_patch_py2 import __author__, __doc__, diff_match_patch, patch_obj
 
-__version__ = "20200712"
+__version__ = "20200713"
 __packager__ = "John Reese (john@noswap.com)"
