67421872cd9d8323b62bf25d3427935f852e3498
John Reese <john@noswap.com>
2020-07-13 00:02:51 -0700

    Formatting

--- diff_match_patch/__init__.py
+++ diff_match_patch/__init__.py
@@ -1,9 +1,9 @@
 import sys
 
 if sys.version_info >= (3, 0):
-    from .diff_match_patch import diff_match_patch, patch_obj, __author__, __doc__
+    from .diff_match_patch import __author__, __doc__, diff_match_patch, patch_obj
 else:
-    from .diff_match_patch_py2 import diff_match_patch, patch_obj, __author__, __doc__
+    from .diff_match_patch_py2 import __author__, __doc__, diff_match_patch, patch_obj
 
 __version__ = "20200712"
 __packager__ = "John Reese (john@noswap.com)"
--- diff_match_patch/tests/speedtest.py
+++ diff_match_patch/tests/speedtest.py
@@ -17,34 +17,38 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import imp
 import gc
+import imp
 import os
 import sys
 import time
+
 parentPath = os.path.abspath("..")
 if parentPath not in sys.path:
     sys.path.insert(0, parentPath)
 import diff_match_patch as dmp_module
+
 # Force a module reload.  Allows one to edit the DMP module and rerun the test
 # without leaving the Python interpreter.
 imp.reload(dmp_module)
 
+
 def main():
-  text1 = open("speedtest1.txt").read()
-  text2 = open("speedtest2.txt").read()
+    text1 = open("speedtest1.txt").read()
+    text2 = open("speedtest2.txt").read()
 
-  dmp = dmp_module.diff_match_patch()
-  dmp.Diff_Timeout = 0.0
+    dmp = dmp_module.diff_match_patch()
+    dmp.Diff_Timeout = 0.0
 
-  # Execute one reverse diff as a warmup.
-  dmp.diff_main(text2, text1, False)
-  gc.collect()
+    # Execute one reverse diff as a warmup.
+    dmp.diff_main(text2, text1, False)
+    gc.collect()
 
-  start_time = time.time()
-  dmp.diff_main(text1, text2, False)
-  end_time = time.time()
-  print("Elapsed time: %f" % (end_time - start_time))
+    start_time = time.time()
+    dmp.diff_main(text1, text2, False)
+    end_time = time.time()
+    print("Elapsed time: %f" % (end_time - start_time))
+
 
 if __name__ == "__main__":
-  main()
+    main()
