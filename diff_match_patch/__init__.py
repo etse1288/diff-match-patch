51914f48fbd0481fc2bc8d3bd74cc1f9e4c045c5
Amethyst Reese <amethyst@n7.gg>
2023-04-30 14:00:42 -0700

    Use attribution for version/changelog

--- diff_match_patch/__init__.py
+++ diff_match_patch/__init__.py
@@ -4,7 +4,7 @@ Repackaging of Google's Diff Match and Patch libraries.
 Offers robust algorithms to perform the operations required for synchronizing plain text.
 """
 
+from .__version__ import __version__
 from .diff_match_patch import __author__, __doc__, diff_match_patch, patch_obj
 
-__version__ = "20200713"
 __packager__ = "Amethyst Reese (amy@noswap.com)"
